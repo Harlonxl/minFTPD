25399fa258e331759c4ef36c74fa215fc337218f
Harlonxl <waitcoding@gmail.com>
2018-07-10 09:33:02 +0800

    modify some bug

--- ftpproto.h
+++ ftpproto.h
@@ -5,5 +5,7 @@
 #include "sysutil.h"
 
 void handle_child(session_t *sess);
+void ftp_replay(session_t *sess, int status, const char *text);
+void ftp_lreplay(session_t *sess, int status, const char *text);
 
 #endif /*_FTP_PROTO_H_*/
